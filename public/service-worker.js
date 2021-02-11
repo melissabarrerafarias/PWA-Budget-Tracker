c52d958498407e1b8179fc90ab6fc13e8c5c9e20
melissikiss <melissikiss@gmail.com>
2021-02-10 16:16:42 -0800

    added service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 const FILES_TO_CACHE = [
-    "index.html", 
-    "./js/idb.js", 
-    "./js/index.js", 
+    "index.html",
+    "./js/idb.js",
+    "./js/index.js",
     "./css/styles.css"
 ];
 const APP_PREFIX = 'BudgetTracker-';
@@ -13,6 +13,35 @@ self.addEventListener('install', function (e) {
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            return request || fetch(e.request)
         })
     )
 })
